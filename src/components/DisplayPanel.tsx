f89e0e8833e59186692e47b793da044457560114
Scott Gibbs <55638818+kangabru@users.noreply.github.com>
2024-01-02 16:24:32 +0100

    Added decrypted text display to the QR code display panel

--- src/components/DisplayPanel.tsx
+++ src/components/DisplayPanel.tsx
@@ -21,19 +21,25 @@ export default function DisplayPanel({
   return (
     <Panel title={title}>
       {qrCodeInfo ? (
-        <div
-          dangerouslySetInnerHTML={{ __html: qrCodeInfo.svgHtml }}
-          className="w-full max-w-lg aspect-square"
-        />
+        <>
+          <div
+            dangerouslySetInnerHTML={{ __html: qrCodeInfo.svgHtml }}
+            className="w-full max-w-lg aspect-square rounded-md shadow-sm border border-gray-300"
+          />
+          <p className="mt-4 mb-1 text-sm">Decrypted text:</p>
+          <div className="text-mono p-2 rounded-md text-sm shadow-sm bg-gray-50 border border-gray-300 break-words">
+            {qrCodeInfo.dataDecrypted}
+          </div>
+        </>
       ) : (
         <div className="grid place-items-center w-full h-full text-gray-200">
           <QrcodeIcon className="max-h-40" />
         </div>
       )}
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4 mt-4">
         <button
-          className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50 bg-blue-500 text-white disabled"
+          className="p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50 bg-blue-500 text-white disabled"
           disabled={!qrCodeInfo}
           onClick={() => {
             const { svgHtml, dataEncrypted } = qrCodeInfo!;
@@ -46,7 +52,7 @@ export default function DisplayPanel({
         </button>
 
         <button
-          className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50 bg-blue-500 text-white disabled"
+          className="p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50 bg-blue-500 text-white disabled"
           disabled={!qrCodeInfo}
           onClick={() => {
             const { svgHtml, dataEncrypted } = qrCodeInfo!;
