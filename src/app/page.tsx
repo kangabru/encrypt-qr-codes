3e9ead4025867266993854b2df5c382b95f38947
Scott Gibbs <55638818+kangabru@users.noreply.github.com>
2024-01-03 18:25:37 +0100

    Added hanging QR codes to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Q_A, faqsHome } from "@/components/Faqs";
 import {
   hrefEncryptionAlgo,
   hrefExamplePython,
@@ -9,6 +8,8 @@ import {
 } from "@/common/links";
 import { CssClass } from "@/common/utils";
 import L from "@/components/FaqLink";
+import { Q_A, faqsHome } from "@/components/Faqs";
+import HangingQrCodes from "@/components/HangingQrCodes";
 import { QrcodeIcon } from "@heroicons/react/outline";
 import {
   CodeIcon,
@@ -23,8 +24,64 @@ import { SVGProps } from "react";
 
 export default function Home() {
   return (
-    <main className="bg-gray-50 min-h-screen w-full flex flex-col items-center p-5 py-12 sm:py-16">
-      <Content />
+    <main className="relative bg-gray-50 min-h-screen w-full flex flex-col items-center p-5 py-12 sm:py-16 border-y-4 border-indigo-500">
+      <HangingQrCodes />
+
+      <section className="relative flex flex-col items-center text-center">
+        <div className="relative z-10 flex flex-col items-center text-center">
+          <QrcodeIcon className="w-20 h-20 text-indigo-500 mb-4" />
+
+          <h1 className="text-3xl font-bold tracking-tight leading-loose text-gray-900 sm:text-4xl">
+            Encrypt QR codes
+            <br />
+            for private backups
+          </h1>
+          <p className="mt-6 max-w-xl text-lg text-gray-600">
+            Securely encrypt and decrypt QR codes used for two-factor
+            authentication. Safely store encrypted QR codes in your photo
+            gallery or print them out.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              href="/encrypt"
+              className="flex items-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <LockClosedIcon className="w-6 h-6 mr-2 -ml-1" />
+              <span>Encrypt</span>
+            </Link>
+            <Link
+              href="/decrypt"
+              className="flex items-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <LockOpenIcon className="w-6 h-6 mr-2 -ml-1" />
+              <span>Decrypt</span>
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+        <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+          {features.map((feature) => (
+            <div key={feature.name} className="relative pl-16">
+              <dt className="text-base font-semibold leading-7 text-gray-900">
+                <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                  <feature.icon
+                    className="h-6 w-6 text-white"
+                    aria-hidden="true"
+                  />
+                </div>
+                {feature.name}
+              </dt>
+              <dd className="mt-2 text-base leading-7 text-gray-600">
+                {feature.description}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </section>
+
+      <FaqLarge faqs={faqsHome} className="mt-20 mx-auto max-w-2xl" />
     </main>
   );
 }
@@ -72,66 +129,6 @@ const features: Feature[] = [
     icon: FingerPrintIcon,
   },
 ];
-
-function Content() {
-  return (
-    <>
-      <section className="flex flex-col items-center text-center mx-auto max-w-2xl">
-        <QrcodeIcon className="w-20 h-20 text-indigo-500 mb-4" />
-
-        <h1 className="text-3xl font-bold tracking-tight leading-loose text-gray-900 sm:text-4xl">
-          Encrypt QR codes
-          <br />
-          for private backups
-        </h1>
-        <p className="mt-6 max-w-xl text-lg text-gray-600">
-          Securely encrypt and decrypt QR codes used for two-factor
-          authentication. Safely store encrypted QR codes in your photo gallery
-          or print them out.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            href="/encrypt"
-            className="flex items-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <LockClosedIcon className="w-6 h-6 mr-2 -ml-1" />
-            <span>Encrypt</span>
-          </Link>
-          <Link
-            href="/decrypt"
-            className="flex items-center rounded-md bg-indigo-600 px-3.5 py-2.5 text-lg font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            <LockOpenIcon className="w-6 h-6 mr-2 -ml-1" />
-            <span>Decrypt</span>
-          </Link>
-        </div>
-      </section>
-
-      <section className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-        <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-          {features.map((feature) => (
-            <div key={feature.name} className="relative pl-16">
-              <dt className="text-base font-semibold leading-7 text-gray-900">
-                <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                  <feature.icon
-                    className="h-6 w-6 text-white"
-                    aria-hidden="true"
-                  />
-                </div>
-                {feature.name}
-              </dt>
-              <dd className="mt-2 text-base leading-7 text-gray-600">
-                {feature.description}
-              </dd>
-            </div>
-          ))}
-        </dl>
-      </section>
-
-      <FaqLarge faqs={faqsHome} className="mt-20 mx-auto max-w-2xl" />
-    </>
-  );
-}
 
 export function FaqLarge({ faqs, className }: { faqs: Q_A[] } & CssClass) {
   return (
---
+++ src/components/HangingQrCodes.tsx
@@ -0,0 +1,56 @@
+import { Children } from "@/common/utils";
+import { QrcodeIcon } from "@heroicons/react/outline";
+import { ReactNode, useMemo } from "react";
+
+export default function HangingQrCodes() {
+  return (
+    <section className="opacity-80 hidden md:block">
+      <QrHang left={10} counts={[5, 15, 10, 5, 15, 10, 15]}>
+        <QrcodeIcon className="w-20 h-20 p-2 scale-90 rotate-6 rounded-lg bg-red-200 text-red-800" />
+      </QrHang>
+      <QrHang left={25} counts={[15, 5, 20]}>
+        <QrcodeIcon className="w-20 h-20 p-2 -rotate-2 rounded-lg bg-blue-200 text-blue-800" />
+      </QrHang>
+      <QrHang left={75} counts={[25, 5, 15, 15]}>
+        <QrcodeIcon className="w-20 h-20 p-2 scale-100 rotate-3 rounded-lg bg-orange-200 text-orange-800" />
+      </QrHang>
+      <QrHang left={90} counts={[5, 5, 15, 10, 15, 20, 5, 10]}>
+        <QrcodeIcon className="w-20 h-20 p-2 scale-95 -rotate-3 rounded-lg bg-pink-200 text-pink-800" />
+      </QrHang>
+    </section>
+  );
+}
+
+function QrHang(props: { left: number; counts: number[] } & Children) {
+  const [lines, h]: [ReactNode[], number] = useMemo(() => {
+    const _lines: ReactNode[] = [];
+
+    const gap = 10;
+    let y0 = 5;
+    for (const count of props.counts) {
+      const y1 = y0 + count;
+      const op =
+        (Math.sin(y1 * 213423 + count * 123242) * 0.5 + 0.5) * 0.8 + 0.2;
+      _lines.push(<line key={y0} x1="5" y1={y0} x2="5" y2={y1} opacity={op} />);
+      y0 = y1 + gap;
+    }
+
+    return [_lines, y0];
+  }, [props.counts]);
+
+  return (
+    <div
+      className="absolute top-0 -translate-x-1/2 flex flex-col items-center text-indigo-500"
+      style={{
+        left: `${props.left}%`,
+      }}
+    >
+      <svg viewBox={`0 0 10 ${h}`} height={h}>
+        <g stroke="currentColor" strokeLinecap="round" strokeWidth={6}>
+          {lines}
+        </g>
+      </svg>
+      {props.children}
+    </div>
+  );
+}
