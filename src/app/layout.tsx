4eef51503bfe2b13eace9a482d915eb81bb44dae
Scott Gibbs <55638818+kangabru@users.noreply.github.com>
2024-01-10 15:25:02 +0100

    Added social image metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,17 @@ export const metadata: Metadata = {
   title: "Encrypt QR Codes",
   description:
     "Securely encrypt and decrypt QR codes used for two-factor authentication. Safely store encrypted QR codes in your photo gallery or print them out.",
+  metadataBase: new URL("https://encrypt-qr-codes.netlify.app/"),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+      "de-DE": "/de-DE",
+    },
+  },
+  openGraph: {
+    images: "/opengraph-image.png",
+  },
 }
 
 export default function RootLayout({ children }: Children) {
